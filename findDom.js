d098fe9b6c0596b52128ec896b316954b35a56b4
Nastasia <nsisco@uw.edu>
2023-11-22 18:38:48 -0800

    adds solution file

--- findDom.js
+++ findDom.js
@@ -22,3 +22,21 @@ window.onload = () => {
 if (typeof module !== 'undefined') {
   module.exports = { getWidth };
 }
+
+
+
+// ------- GIVEN SOLUTION FILE:
+// function getWidth(id) {
+//   // Return the width of the element specified in the parameter "id"
+//   const areaWidth = document.getElementById(id).clientWidth;
+//   return areaWidth + ' px';
+// }
+
+// // No need to change below this point.
+// // Note how we're using DOM elements here to display the result of the getWidth() function inside the "areaWidth" element
+// window.onload = () => {
+//   document.getElementById('areaWidth').innerHTML = getWidth('mainDiv');
+// };
+
+// //don't change this line
+//   module.exports = { getWidth };
